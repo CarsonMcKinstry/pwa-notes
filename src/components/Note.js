91596d0b5c0b9031fb26fbe7b4e172342f383f31
Carson McKinstry <carson.mckinstry@gmail.com>
2018-06-24 15:47:23 -0500

    its starting to come together, is?

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,6 +3,25 @@ import {PropTypes} from 'prop-types';
 import Swipeable from 'react-swipeable';
 import styled, { keyframes } from 'styled-components';
 import { Icon } from 'rmwc/Icon';
+import faker from 'faker';
+import moment from 'moment';
+import DebouncedInput from 'react-debounce-input';
+
+function debounce(fn, w, imm) {
+  let timeout;
+  return function() {
+    let context = this;
+    let args = arguments;
+    let later = function() {
+      timeout = null;
+      if (!imm) fn.apply(context, args);
+    }
+    let callNow = imm && !timeout;
+    clearTimeout(timeout);
+    timeout - setTimeout(later, w)
+    if (callNow) fn.apply(context, args);
+  }
+}
 
 const breakpoint = '600px';
 
@@ -25,6 +44,7 @@ const SavingIcon = styled(({isSaving, ...props}) => <Icon {...props}/>)`
 `;
 
 const NotePage = styled(Swipeable)`
+  position: relative;
   height: calc(100% - 56px);
   @media screen and (min-width: ${breakpoint}) {
     height: calc(100% - 64px);
@@ -32,27 +52,55 @@ const NotePage = styled(Swipeable)`
 `;
 
 const NoteTopBar = styled.div`
+  position: relative;
   height: 36px;
   padding: 0 12px;
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
+
+`
+
+const NoteArea = styled(props => <DebouncedInput {...props} element="textarea"/>)`
+  resize: none;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1em;
+  line-height: 1.5;
+  height: calc(100% - 36px);
+  box-sizing: border-box;
+  padding: 6px 12px;
+  width: 100%;
+  border: 0;
+  &:focus {
+    outline: 0;
+    border: 0;
+  }
+`;
+
+const NoteDate = styled.p`
+  color: rgba(0,0,0,0.54);
+  font-size: 90%;
+`
+
+const NoteStatusIcons = styled.div`
+  color: rgba(0,0,0,0.54);
 `
 
 class Note extends Component {
 
   state = {
-    isSaving: true
+    isSaving: false,
+    note: faker.lorem.paragraphs(5),
   }
 
   componentWillMount() {
     this.props.setTitle('Note')
+  }
 
-    setTimeout(() => {
-      this.setState({
-        isSaving: false
-      });
-    }, 3000); 
+  componentWillUnmount() {
+    this.setState({
+      tm: null
+    })
   }
 
   handleSwipeRight = (e, d, f) => {
@@ -61,17 +109,41 @@ class Note extends Component {
     }
   }
 
+  handleNoteChange = (e) => {
+    e.persist();
+    this.setState({
+      note: e.target.value,
+      isSaving: true
+    }, () => {
+      setTimeout(() => {
+        this.setState({
+          isSaving: false
+        })
+      }, 3000);
+    })
+  }
+
   render(){
     return(
       <NotePage
         onSwipedRight={this.handleSwipeRight}
       >
         <NoteTopBar>
-          <SavingIcon 
-            isSaving={this.state.isSaving} 
-            use={this.state.isSaving ? 'autorenew' : 'done'}
-          />
+          <NoteDate>
+            { moment().format('ddd, D MMM. \'YY @ H:mm')}
+          </NoteDate>
+          <NoteStatusIcons>
+            <SavingIcon 
+              isSaving={this.state.isSaving} 
+              use={this.state.isSaving ? 'autorenew' : 'done'}
+            />
+          </NoteStatusIcons>
         </NoteTopBar>
+        <NoteArea 
+          value={this.state.note}
+          debounceTimeout={1000}
+          onChange={this.handleNoteChange}
+        />
       </NotePage>
     );
   }
