f1382b9f7d5b32af8f92e42b20e151e9bed4c026
Carson McKinstry <carson.mckinstry@gmail.com>
2018-06-26 17:07:06 -0500

    started testing

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
+global.sinon = sinon;
--- src/components/styled/__tests__/NotePage.spec.jsx
+++ src/components/styled/__tests__/NotePage.spec.jsx
@@ -1,9 +1,72 @@
-/* global it, describe, expect, shallow, render, mount */
+/* global beforeEach, it, describe, expect, shallow, sinon */
 import React from 'react';
 
 import {
   NotePage,
   NoteArea,
   NoteTopBar,
-  NoteStatusIcons
+  NoteStatusIcons,
+  NoteDate,
+  OpenMenu,
+  PlacedMenu,
+  StyledMarkdown
 } from '../NotePage';
+
+describe('<NotePage />', () => {
+  it('should render without crashing', () => {
+    const tree = shallow(<NotePage />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render children', () => {
+    const tree = shallow(
+      <NotePage>
+        <div />
+      </NotePage>
+    );
+
+    expect(tree.find('div').length).toEqual(1);
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<NoteArea />', () => {
+  it('should render without crashing', () => {
+    const tree = shallow(<NoteArea />);
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<NoteTopBar />', () => {
+  it('should render without crashing', () => {
+    const tree = shallow(<NoteTopBar />);
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<NoteStatusIcons />', () => {
+  it('should render without crashing', () => {
+    const tree = shallow(
+      <NoteStatusIcons>
+        menu
+      </NoteStatusIcons>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<NoteStatusIcons />', () => {
+  it('should render without crashing', () => {
+    const tree = shallow(
+      <NoteStatusIcons>
+        menu
+      </NoteStatusIcons>
+    );
+
+    expect(tree).toMatchSnapshot();
+  });
+});
