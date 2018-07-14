c1afea70545ac38fef41cae51f44eb26e4e9746f
Carson McKinstry <carson.mckinstry@gmail.com>
2018-07-14 13:59:31 -0500

    Using the pregenerated precache stuff from workbox

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
 
-workbox.precaching.precacheAndRoute(self.__precacheManifest || [], {});
-
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
