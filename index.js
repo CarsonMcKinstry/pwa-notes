f3f20ebeb80973d698aebac03301e45ecd1a50fe
Carson McKinstry <carson.mckinstry@gmail.com>
2018-07-14 19:04:20 -0500

    why you no work

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const express = require('express');
 const path = require('path');
 
@@ -8,5 +9,13 @@ const port = process.env.PORT || 8080;
 const buildPath = path.resolve(__dirname, './build');
 
 app.use(express.static(buildPath));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './build/index.html'), (err) => { 
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
