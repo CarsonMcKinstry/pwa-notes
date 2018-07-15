87d51dc7489e7d4276c2f7d0a6de491da09ef609
Carson McKinstry <carson.mckinstry@gmail.com>
2018-07-14 19:34:21 -0500

    let 's see what happens

--- src/sw.js
+++ src/sw.js
@@ -4,6 +4,16 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-self.addEventListener('fetch', (e) => {
-  console.log(e);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
