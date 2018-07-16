dc00b31d71b0876ba0e317a6a3868a735e276031
Carson McKinstry <carson.mckinstry@gmail.com>
2018-07-15 19:11:47 -0500

    maybe?

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,19 @@
 /* eslint-disable */
 
+importScripts(
+  '/workbox-v3.3.1/workbox-background-sync.prod.js',
+  '/workbox-v3.3.1/workbox-broadcast-cache-update.prod.js',
+  '/workbox-v3.3.1/workbox-cache-expiration.prod.js',
+  '/workbox-v3.3.1/workbox-cacheable-response.prod.js',
+  '/workbox-v3.3.1/workbox-core.prod.js',
+  '/workbox-v3.3.1/workbox-google-analytics.prod.js',
+  '/workbox-v3.3.1/workbox-precaching.prod.js',
+  '/workbox-v3.3.1/workbox-range-requests.prod.js',
+  '/workbox-v3.3.1/workbox-routing.prod.js',
+  '/workbox-v3.3.1/workbox-strategies.prod.js',
+  '/workbox-v3.3.1/workbox-streams.prod.js',
+);
+
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
 workbox.precaching.suppressWarnings();
